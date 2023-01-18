0f58a8676f7fbd2c261807cb62a15d2844718a4b
Osama Sultan <59367747+Osama-Sultan189@users.noreply.github.com>
2023-01-18 18:46:59 +0500

    created action and reducers

---
+++ src/Components/Shop.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Shop() {
+  return (
+    <div>
+      <button className='btn btn-primary'>-</button>
+      Add to Cart
+      <button className='btn btn-primary'>+</button>
+    </div>
+  )
+}
+
+export default Shop
